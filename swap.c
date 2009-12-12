9550e07562ab61b555d88e39675d1b57572b88e3
takc <takeshi@ubuntu.ubuntu-domain>
2009-12-12 17:11:54 +0900

    swap.cに入力ファイルを付けた。出力ファイルは毎回デフォルトするようにした

--- swap.c
+++ swap.c
@@ -6,16 +6,27 @@
  */
 void* swap(char* s,int a, int alen, int b, int blen);
 
-int main(){
+int main(int argc, char *argv[]){
 	int i,j,k;
 	char s[1000];
 	char *new;
 	int a=15,alen=15,b=35,blen=15; //a:始点 alen:前半の長さ b:後半の始点 blen:後半の長さ
-	FILE *fp;
-	if((fp = fopen("original.txt","r")) == NULL){
+	FILE *fp,*fp1,*fp2;
+	if(argc != 2) printf("please input 1 file");
+	if((fp = fopen(argv[1],"r")) == NULL){
 		printf("file can't open\n");
 		exit(EXIT_FAILURE);
 	}
+	if((fp1 = fopen("train1.txt","w")) == NULL){
+		printf("file can't open\n");
+		exit(EXIT_FAILURE);
+	}
+	fclose(fp1);
+	if((fp2 = fopen("train2.txt","w")) == NULL){
+		printf("file can't open\n");
+		exit(EXIT_FAILURE);
+	}
+	fclose(fp2);
 	while(fgets(s,256,fp) != NULL) swap(s,a,alen,b,blen);
 	fclose(fp);
 	return 0;
@@ -30,12 +41,16 @@ void* swap(char* s,int a, int alen, int b, int blen){
 	//bは始まる方。
 	int i;
 	int n=strlen(s);
-	FILE *fp;
-	//毎回ファイルオープンするのは健全じゃないよな。
-	if((fp = fopen("train.txt","a")) == NULL){
+	FILE *fp1,*fp2;
+
+	if((fp1 = fopen("train1.txt","a")) == NULL){
 		printf("file can't open\n");
 		exit(EXIT_FAILURE);
-	}
+	}	
+	if((fp2 = fopen("train2.txt","a")) == NULL){
+		printf("file can't open\n");
+		exit(EXIT_FAILURE);
+	}	
 	//入れ替える部分を取っておく
 	char mae[a+blen];
 	char usiro[n-b-blen+alen];
@@ -43,14 +58,15 @@ void* swap(char* s,int a, int alen, int b, int blen){
 	for(i=0;i<a;i++)		mae[i]	= s[4+i];
 	for(i=0;i<blen;i++)		mae[a+i]= s[4+b+i];
 	mae[a+i] = '\0';
-	fprintf(fp,"%c1 #%s\n",s[0],mae);
+	fprintf(fp1,"%c1 #%s\n",s[0],mae);
 	//for(i=0;i<b-a-alen;i++)	new[a+blen+i]		= s[a+alen+i];
 	//後半をusiroにコピー
 	for(i=0;i<alen;i++)		usiro[i]		= s[4+a+i];
 	for(i=0;i<n;i++)		usiro[alen+i]	= s[4+b+blen+i];
 	usiro[alen+i] = '\0';
-	fprintf(fp,"%c1 #%s",s[0],usiro);
-	fclose(fp);	
+	fprintf(fp2,"%c1 #%s",s[0],usiro);
+	fclose(fp1);	
+	fclose(fp2);	
 	return;
 }
 
