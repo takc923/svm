202bc142053a8409d8c41ec7aa55c18f817f8426
Takeshi <takc923@gmail.com>
2009-12-13 16:00:48 +0900

    output3に足した数を出力するとこまで。

--- cal_total.c
+++ cal_total.c
@@ -7,11 +7,14 @@
 
 int main(){
 	int i,j,k;
+	int total;
 	char s[1000],t[1000];
 	char *new;
 	char *space;
-	FILE *model1,*model2,*output1,*output2;
-	
+	FILE *model1,*model2,*output1,*output2,*output3;
+	float told;//thresh old
+
+	//最初に全てファイルオープン
 	if((model1 = fopen("model1.txt","r")) == NULL){
 		printf("file can't open\n");
 		exit(EXIT_FAILURE);
@@ -28,7 +31,12 @@ int main(){
 		printf("file can't open\n");
 		exit(EXIT_FAILURE);
 	}
-	for(i=0;i!=11;i++){
+	if((output3 = fopen("output3.txt","w")) == NULL){
+		printf("file can't open\n");
+		exit(EXIT_FAILURE);
+	}
+	//配列の数を取得
+	for(i=0;i!=9;i++){
 		fgets(s,256,model1);
 		fgets(t,256,model2);
 	}
@@ -36,12 +44,31 @@ int main(){
 	*space = '\0';
 	space = strchr(t,' ');	
 	*space = '\0';
-	printf("%s\n%s\n",s,t);
+	total = atoi(s);
+
+	//閾値を取得
+	for(i=0;i!=2;i++){
+		fgets(s,256,model1);
+		fgets(t,256,model2);
+	}
+	space = strchr(s,' ');	
+	*space = '\0';
+	space = strchr(t,' ');	
+	*space = '\0';
+	told = atof(s)+atof(t);
+
 	fclose(model1);
 	fclose(model2);
+
+	for(i=0;i<total;i++){
+		fgets(s,256,output1);
+		fgets(t,256,output2);
+		fprintf(output3,"%f\n",atof(s)+atof(t));
+	}
 
 	
 	fclose(output1);
 	fclose(output2);
+	fclose(output3);
 	return 0;
 }
