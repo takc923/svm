f054aab4039b1018fddd80abdcbed32065736cf3
Takeshi <takc923@gmail.com>
2009-12-14 11:15:17 +0900

    やった！オンメモリで動いた！

--- kernel.h
+++ kernel.h
@@ -35,67 +35,101 @@
 
 double k_value(char* a, char* b, int len, int N);
 char pair(char* a);
+void swap(char* s,int a, int alen, int b, int blen);
 
 double custom_kernel(KERNEL_PARM *kernel_parm, SVECTOR *a, SVECTOR *b) 
      /* plug in you favorite kernel */                          
 {
-  double kernel_value = 0;
-  int N = atoi(kernel_parm->custom);
-  int N_c = N/2;
-  int n_c, m_c;
-  int i, j, n, m;
-  char* x;
-  char* y;
-  int len_a = strlen(a->userdefined);
-  int len_b = strlen(b->userdefined);
-  // printf("%d\n",N);
- 
-  if(len_a == 0 || len_b ==0){
-    return((double)0);
-  }
-  // printf("%d\n",len_a);
+	double kernel_value = 0;
+	int N = atoi(kernel_parm->custom);
+	int N_c = N/2;
+	int n_c, m_c;
+	int i, j, n, m;
+	char* x;
+	char* y;
+	int len_a = strlen(a->userdefined);
+	int len_b = strlen(b->userdefined);
+	int aa=15,alen=15,bb=35,blen=15; //a:始点 alen:前半の長さ b:後半の始点 blen:後半の長さ
+
+	if(len_a == 0 || len_b ==0){
+		return((double)0);
+	}
+	// printf("%d\n",len_a);
 
 
-  if(len_a >= len_b){
-    m = len_a;
-    n = len_b;
-    x = b->userdefined;
-    y = a->userdefined;
-  }
-  else{
-    m = len_b;
-    n = len_a;
-    x = a->userdefined;
-    y = b->userdefined;
-  }
-
-  n_c = n/2;
-  m_c = m/2;
+	if(len_a >= len_b){
+		n = len_b;
+		m = len_a;
+		x = b->userdefined;
+		y = a->userdefined;
+	}
+	else{
+		n = len_a;
+		m = len_b;
+		x = a->userdefined;
+		y = b->userdefined;
+	}
+	char xx[n],yy[m];
+	strcpy(xx,x);strcpy(yy,y);
+	swap(xx,aa,alen,bb,blen);swap(yy,aa,alen,bb,blen);
+	char *xzenhan,*xkouhan,*yzenhan,*ykouhan;
+	//char xzenhan[a+blen+1],xkouhan[n-blen-b+a+1];
+	//char yzenhan[a+blen+1],ykouhan[m-blen-b+a+1];
+	xzenhan = xx;	yzenhan = yy;
+	xzenhan[aa+blen] = '\0';	yzenhan[aa+blen] = '\0';
+	xkouhan = &xx[bb-alen+blen];
+	ykouhan = &yy[bb-alen+blen];
+	
+	n_c = n/2;
+	m_c = m/2;
 
   if(N>n){
     if(N>m){
-      kernel_value = k_value(x, y+(m_c)-(n_c), n, n);
+      kernel_value = k_value(xzenhan, yzenhan+(m_c)-(n_c), n, n);
     }
     else{
       for(i=(N_c)-(n_c); i<=m-N+(N_c)-(n_c); i++){
-	kernel_value += k_value(x, y+i, n, n);
+	kernel_value += k_value(xzenhan, yzenhan+i, n, n);
       }
     }
   }
   else{
     for(i=(N-n); i<=(m-N);i++){
       if(i<0){
-	kernel_value += k_value(x-i, y, n+i, N);
+	kernel_value += k_value(xzenhan-i, yzenhan, n+i, N);
       }
       if(i>=0 && i<=(m-n)){
-	kernel_value += k_value(x, y+i, n, N);
+	kernel_value += k_value(xzenhan, yzenhan+i, n, N);
       }
       if(i>(m-n)){
-	kernel_value += k_value(x, y+i, m-i, N);
+	kernel_value += k_value(xzenhan, yzenhan+i, m-i, N);
       }
     }
   }
 
+  if(N>n){
+    if(N>m){
+      kernel_value = k_value(xkouhan, ykouhan+(m_c)-(n_c), n, n);
+    }
+    else{
+      for(i=(N_c)-(n_c); i<=m-N+(N_c)-(n_c); i++){
+	kernel_value += k_value(xkouhan, ykouhan+i, n, n);
+      }
+    }
+  }
+  else{
+    for(i=(N-n); i<=(m-N);i++){
+      if(i<0){
+	kernel_value += k_value(xkouhan-i, ykouhan, n+i, N);
+      }
+      if(i>=0 && i<=(m-n)){
+	kernel_value += k_value(xkouhan, ykouhan+i, n, N);
+      }
+      if(i>(m-n)){
+	kernel_value += k_value(xkouhan, ykouhan+i, m-i, N);
+      }
+    }
+  }
   //printf("%f\n",kernel_value);
   return(kernel_value); 
 }
@@ -145,4 +179,16 @@ char pair(char* a)
   }
 }
 
+void swap(char* s,int a, int alen, int b, int blen){
+	int i;
+	int n=strlen(s);
+	char tmp[n];
 
+	for(i=0;i<a;i++)		tmp[i]				= s[i];
+	for(i=0;i<blen;i++)		tmp[a+i]			= s[b+i];
+	for(i=0;i<b-a-alen;i++)	tmp[a+blen+i] 		= s[a+alen+i];
+	for(i=0;i<blen;i++)		tmp[b-alen+blen+i]	= s[b+i];
+	for(i=0;i<n-b-blen;i++)	tmp[b+blen+i]		= s[b+blen+i];
+	for(i=0;i<n;i++)		s[i]				= tmp[i];
+	return;
+}
