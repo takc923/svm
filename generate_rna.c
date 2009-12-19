2ae5e5ea7bd8de7c8296cfddac9e3103f570831a
Takeshi <takc923@gmail.com>
2009-12-20 00:49:37 +0900

    一段落

--- generate_rna.c
+++ generate_rna.c
@@ -7,15 +7,10 @@
 #define ALEN 6
 #define B 18
 #define BLEN 7
-
-char* aucg(int c){
-	switch(c){
-		case '0':return"A";
-		case '1':return"U";
-		case '2':return"C";
-		case '3':return"G";
-	}
-}
+#define L 35
+void generate_positive(FILE *fp,int n);
+void generate_negative(FILE *fp,int n);
+char aucg();
 
 int main(int argc, char *argv[]){
 	int i,j;
@@ -25,22 +20,81 @@ int main(int argc, char *argv[]){
 	FILE *fp;
 	srand(time(NULL));	
 	fp = fopen("rna.txt","w");
-	if(argc != 3){
-		printf("please input number and length.\n");
+	if(argc != 2){
+		printf("please input number.\n");
 		exit(1);
 	}
-	for(j=0;j<atoi(argv[1]);j++){
-		for(i=0;i<atoi(argv[2]);i++){
-			r = rand()%4;
-			printf("aaa\n");
-			tmp = aucg(r);
-			printf("%s\n",tmp);
-			fputs(tmp,fp);
-			printf("bbb\n");
-		}
-		fputs("\n",fp);
-	}
+	int n = atoi(argv[1]);//作る数。
+	int half = (int)(n/2);
+	printf("half=%d\n",half);
+	printf("a\n");
+	generate_positive(fp,half);
+	generate_negative(fp,half);
+	printf("a\n");
 	fclose(fp);
 }
 
+//fp:入力するファイルポインタ,n:作る数
+void generate_positive(FILE *fp,int n){
+	int i,j;
+	char l1[A],l2[ALEN],l3[B-A-ALEN],l4[BLEN],l5[L-B-BLEN],l6[L-B-BLEN-ALEN];
+	for(j=0;j<n;j++){
+		for(i=0;i<A;i++){
+			l1[i] = aucg();
+		}
+		for(i=0;i<ALEN;i++){
+			l2[i] = aucg();
+		}
+		for(i=0;i<B-A-ALEN;i++){
+			l3[i] = aucg();
+		}
+		for(i=0;i<BLEN;i++){
+			l4[i] = l1[A-1-i];
+		}
+		for(i=0;i<ALEN;i++){
+			l5[i] = l2[ALEN-1-i];
+		}
+		for(i=0;i<L-B-BLEN-ALEN;i++){
+			l6[i] = aucg();
+		}
+		fprintf(fp,"+1 #%s%s%s%s%s\n",l1,l2,l3,l4,l5);
+	}
+}
 
+//fp:入力するファイルポインタ,n:作る数
+void generate_negative(FILE *fp,int n){
+	int i,j;
+	char l1[A],l2[ALEN],l3[B-A-ALEN],l4[BLEN],l5[L-B-BLEN],l6[L-B-BLEN-ALEN];
+	for(j=0;j<n;j++){
+		for(i=0;i<A;i++){
+			l1[i] = aucg();
+		}
+		for(i=0;i<ALEN;i++){
+			l2[i] = aucg();
+		}
+		for(i=0;i<B-A-ALEN;i++){
+			l3[i] = aucg();
+		}
+		for(i=0;i<BLEN;i++){
+			l4[i] = aucg();
+		}
+		for(i=0;i<ALEN;i++){
+			l5[i] = aucg();
+		}
+		for(i=0;i<L-B-BLEN-ALEN;i++){
+			l6[i] = aucg();
+		}
+		fprintf(fp,"+1 #%s%s%s%s%s\n",l1,l2,l3,l4,l5);
+	}
+}
+//AUCGのいずれかをランダムで出力する。
+char aucg(){
+	int r = rand()%4;
+	char c[1];
+	switch(r){
+		case '0': return 'A';
+		case '1': return 'U';
+		case '2': return 'C';
+		case '3': return 'G';
+	}
+}
